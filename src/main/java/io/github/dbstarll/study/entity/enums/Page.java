1e4238b87dc7d8e5c344f8d3ea228115e15dc4dc
dbstarll <dbstarll@gmail.com>
2023-04-23 17:26:56 +0800

    迁移Subscribe、SubscribeService

---
+++ src/main/java/io/github/dbstarll/study/entity/enums/Page.java
@@ -0,0 +1,70 @@
+package io.github.dbstarll.study.entity.enums;
+
+import io.github.dbstarll.utils.lang.enums.EnumValue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@EnumValue(method = "toString")
+public enum Page {
+    // ENGLISH
+    // word("word",Module.ENGLISH),
+
+    BOOK("book", Module.ENGLISH),
+
+    EXERCISE_BOOK("exercise_book", Module.ENGLISH, Mode.USER),
+
+    SPELL("spell", Module.ENGLISH, Mode.USER, Mode.GUEST),
+
+    SPELL_EXCHANGE("spell_exchange", Module.ENGLISH, Mode.USER),
+
+    // listen(Module.ENGLISH, Mode.USER),
+
+    // read(Module.ENGLISH, Mode.USER),
+
+    // MATH
+    // custom(Module.MATH, Mode.USER),
+
+    // mix(Module.MATH, Mode.USER, Mode.GUEST),
+
+    // SETTING
+
+    USER("user", Module.SETTING);
+
+    // approve(Module.SETTING);
+
+    private final String name;
+    private final Module module;
+    private final Set<Mode> modes;
+
+    Page(final String name, final Module module, final Mode... modes) {
+        this.name = name;
+        this.module = module;
+        this.modes = new HashSet<>(Arrays.asList(modes));
+    }
+
+    /**
+     * 获得Page所属的Module.
+     *
+     * @return module
+     */
+    public Module getModule() {
+        return module;
+    }
+
+    /**
+     * 检查指定的用户模式是否能访问page.
+     *
+     * @param mode 用户模式
+     * @return 是否能访问page
+     */
+    public boolean allowMode(final Mode mode) {
+        return modes.contains(mode);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
