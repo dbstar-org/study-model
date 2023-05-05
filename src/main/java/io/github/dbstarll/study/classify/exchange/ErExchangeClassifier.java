fc4e821431db76a1dde99f01cab80246d7ee5562
dbstarll <dbstarll@gmail.com>
2023-05-05 13:40:40 +0800

    合并重复代码

--- src/main/java/io/github/dbstarll/study/classify/exchange/ErExchangeClassifier.java
+++ src/main/java/io/github/dbstarll/study/classify/exchange/ErExchangeClassifier.java
@@ -6,16 +6,20 @@ package io.github.dbstarll.study.classify.exchange;
 class ErExchangeClassifier implements ExchangeClassifier {
     @Override
     public String classify(final String word, final String exchange) {
-        if (exchange.equals(word + "er")) {
+        return classify(word, exchange, "er", "more");
+    }
+
+    static String classify(final String word, final String exchange, final String suffix, final String prefix) {
+        if (exchange.equals(word + suffix)) {
             return null;
-        } else if (exchange.equals("more " + word)) {
+        } else if (exchange.equals(prefix + " " + word)) {
             return null;
-        } else if (word.endsWith("y") && exchange.equals(word.substring(0, word.length() - 1) + "ier")) {
-            return "yer";
-        } else if (word.endsWith("e") && exchange.equals(word + "r")) {
-            return "eer";
-        } else if (exchange.equals(word + word.substring(word.length() - 1) + "er")) {
-            return "2er";
+        } else if (word.endsWith("y") && exchange.equals(word.substring(0, word.length() - 1) + "i" + suffix)) {
+            return "y" + suffix;
+        } else if (word.endsWith("e") && exchange.equals(word + suffix.substring(1))) {
+            return "e" + suffix;
+        } else if (exchange.equals(word + word.substring(word.length() - 1) + suffix)) {
+            return "2" + suffix;
         } else if (exchange.equals(word)) {
             return "same";
         } else {
